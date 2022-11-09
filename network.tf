a11aabae881b1ab5970c2c8342cab3305e6d3499
paulpfleg <98637172+paulpfleg@users.noreply.github.com>
2022-11-09 17:09:26 +0100

    mit network.tf

---
+++ network.tf
@@ -0,0 +1,70 @@
+# --- Network ---
+
+# Create the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = local.default_vpc_cidr
+  enable_dns_hostnames = true
+}
+# Define the public subnet
+resource "aws_subnet" "public-subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = local.frontend_subnet_cidr
+  availability_zone = local.frontend_subnet_az
+}
+# Define the internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+}
+# Define the public route table
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+# Assign the public route table to the public subnet
+resource "aws_route_table_association" "public-rt-association" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+# Define the security group for the EC2 Instance
+resource "aws_security_group" "aws-vm-sg" {
+  name        = "vm-sg"
+  description = "Allow incoming connections"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming HTTP connections"
+  }
+
+    ingress {
+    from_port   = 8080
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming HTTP connections"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming SSH connections"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "frontend"
+  }
+}
